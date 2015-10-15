76cd4028d119dd1f141d14000dad504a7b563db9
liudidi <liudidi@gmail.co>
2015-10-15 00:53:54 +0000

    弄错了。

--- src/mlimecfg.cpp
+++ src/mlimecfg.cpp
@@ -1,112 +1,68 @@
-/***************************************************************************
- *   Copyright (C) 2008 by lovewilliam   *
- *   lovewilliam@gmail.com   *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- ***************************************************************************/
-
-
-#include <ntqlayout.h>
-
-#include <tdelocale.h>
-#include <tdeglobal.h>
-#include <tdeparts/genericfactory.h>
-
-#include <kdebug.h>
-
 #include "mlimecfg.h"
 
-static const char description[] =
-    I18N_NOOP("This module help you to manage your input methods");
+#include <QVBoxLayout>
 
-static const char version[] = "1.0.0";
+#include <KGenericFactory>
+#include <KLocale>
+#include <KGlobal>
+#include <KDialog>
 
-typedef KGenericFactory<mlimecfg, TQWidget> mlimecfgFactory;
-K_EXPORT_COMPONENT_FACTORY( kcm_mlimecfg, mlimecfgFactory("mlimecfg"))
+K_PLUGIN_FACTORY( MLImeCfgFactory, registerPlugin<MLImeCfg>(); )
+K_EXPORT_PLUGIN( MLImeCfgFactory( "kcmmlimecfg", "kcm_mlimecfg" ) )
 
-mlimecfg::mlimecfg(TQWidget *parent, const char *name, const TQStringList&)
-    : TDECModule(parent, name), myAboutData(0)
+MLImeCfg::MLImeCfg( QWidget *parent, const QVariantList &args )
+    : KCModule( MLImeCfgFactory::componentData(), parent, args )
 {
+    QVBoxLayout* layout = new QVBoxLayout( this );
+    layout->setMargin( 0 );
+    layout->setSpacing( KDialog::spacingHint() );
 
-    TDEAboutData *about =
-    new TDEAboutData(I18N_NOOP("mlimecfg"), I18N_NOOP("Magic Linux IME Module"),
-                  version, description, TDEAboutData::License_GPL,
-                  I18N_NOOP("(c) 2005-2008 lovewilliam"));
+    m_micl = new MLImeCfg_CL( this );
+    layout->addWidget( m_micl );
+    m_micl->show();
 
-	about->addAuthor("KanKer",0,"kanker@163.com");
-	about->addAuthor("lovewilliam", 0, "lovewilliam@gmail.com");
+    KAboutData *about =
+            new KAboutData( "IME Configure", 0, ki18n( "Magic Linux IME Configure Module" ), "1.5.0",
+                            ki18n( "This module help you to manage your input methods" ),
+                            KAboutData::License_GPL,
+                            ki18n( "(c) 2005-2009 Magic Group" ) );
 
-	setAboutData( about );
+    about->addAuthor( ki18n( "KanKer" ), ki18n( "IME configure scripts" ), "kanker@163.com" );
+    about->addAuthor( ki18n( "lovewilliam" ), ki18n( "kcm module" ), "lovewilliam@gmail.com" );
+    about->addAuthor( ki18n( "Ni Hui" ), ki18n( "Port to cmake, Port to Qt4/KDE4, Port to new kpluginfactory" ), "shuizhuyuanluo@126.com" );
+    setAboutData( about );
 
     // place widgets here
-	load();
+    load();
 };
 
-
-mlimecfg::~mlimecfg()
+MLImeCfg::~MLImeCfg()
 {
+    delete m_micl;
 }
 
-
-void mlimecfg::load()
+void MLImeCfg::load()
 {
-	micl = new mlimecfg_cl(this,i18n("Input Method"));
-
-	TQGridLayout *grid = new TQGridLayout( this, 1, 1 );
-	grid->addWidget( micl,   0, 0 );
-
-	micl->show();
-	micl->adjustSize();
-
+    // insert your loading code here...
 }
 
-
-void mlimecfg::defaults()
+void MLImeCfg::defaults()
 {
     // insert your default settings code here...
     emit changed(true);
 }
 
-
-void mlimecfg::save()
+void MLImeCfg::save()
 {
     // insert your saving code here...
-	micl->buttonOk_clicked();
-	emit changed(false);
+    m_micl->buttonOk_clicked();
+    emit changed(false);
 }
 
-
-int mlimecfg::buttons()
-{
-	return TDECModule::Default|TDECModule::Apply|TDECModule::Help;
-
-}
-
-
-void mlimecfg::configChanged()
+void MLImeCfg::configChanged()
 {
     // insert your saving code here...
     emit changed(true);
 }
-
-
-TQString mlimecfg::quickHelp() const
-{
-    return description;
-}
-
 
 #include "mlimecfg.moc"
--- src/mlimecfg.h
+++ src/mlimecfg.h
@@ -1,55 +1,28 @@
-/***************************************************************************
- *   Copyright (C) 2008 by lovewilliam   *
- *   lovewilliam@gmail.com   *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- ***************************************************************************/
-
-
 #ifndef _MLIMECFG_H_
 #define _MLIMECFG_H_
 
-#include <tdecmodule.h>
-#include <tdeaboutdata.h>
+#include <KCModule>
+#include <KAboutData>
+
 #include "mlimecfg_cl.h"
 
-class mlimecfg: public TDECModule
+class MLImeCfg : public KCModule
 {
     Q_OBJECT
 
-public:
-    mlimecfg( TQWidget *parent=0, const char *name=0, const TQStringList& = TQStringList() );
-    ~mlimecfg();
+    public:
+        MLImeCfg( QWidget *parent, const QVariantList &args );
+        ~MLImeCfg();
 
-    virtual void load();
-    virtual void save();
-    virtual void defaults();
-    virtual int buttons();
-    virtual TQString quickHelp() const;
-    virtual const TDEAboutData *aboutData()const
-    { return myAboutData; };
+        void load();
+        void save();
+        void defaults();
 
-public slots:
-    void configChanged();
+    public slots:
+        void configChanged();
 
-private:
-
-	mlimecfg_cl *micl;
-	TDEAboutData *myAboutData;
-
+    private:
+        MLImeCfg_CL* m_micl;
 };
 
-#endif
+#endif // _MLIMECFG_H_
